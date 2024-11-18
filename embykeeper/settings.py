4b8d43fd82ea626e48d3a3b1c71915acb9107076
jackzzs <jackzzs@outlook.com>
2024-11-18 19:37:11 +0800

    Allow emby login only

--- embykeeper/settings.py
+++ embykeeper/settings.py
@@ -77,7 +77,7 @@ def check_config(config):
                         ),
                         "username": str,
                         "password": str,
-                        Optional("time"): Or(PositiveInt(), [PositiveInt()]),
+                        Optional("time"): Or(int, [PositiveInt()]),
                         Optional("continuous"): bool,
                         Optional("jellyfin"): bool,
                         Optional("ua"): str,
@@ -446,7 +446,7 @@ async def prepare_config(config_file=None, basedir=None, public=False, windows=F
         if "should be instance of 'int'" in error_str:
             error_str += f"\n说明: 您可能对需要使用数值的参数设置了字符串, 请去除引号 (xxx = 1234)."
         logger.error(
-            f"配置文件错误, 请检查配置文件 (参考: https://emby-keeper.github.io/guide/配置指南):\n{error_str}"
+            f"配置文件错误, 请检查配置文件 (参考: https://emby-keeper.github.io/guide/配置文件):\n{error_str}"
         )
         sys.exit(253)
     proxy: dict = config.get("proxy", None)
