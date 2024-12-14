6f26c753ac4a8c7edb2b3795e6163a0122f4eaa5
jackzzs <jackzzs@outlook.com>
2024-12-14 17:09:25 +0800

    Formatting

--- embykeeper/telechecker/tele.py
+++ embykeeper/telechecker/tele.py
@@ -45,8 +45,8 @@ from embykeeper.utils import async_partial, show_exception, to_iterable
 
 logger = logger.bind(scheme="telegram")
 
-_id = b'\x80\x04\x95\x15\x00\x00\x00\x00\x00\x00\x00]\x94(K2K3K7K8K5K8K4K6e.'
-_hash = b'\x80\x04\x95E\x00\x00\x00\x00\x00\x00\x00]\x94(KbKdK7K4K0KeKaK2KaKcKeKeK7K3K9K0KeK0KbK3K5K4KeKcK8K0K9KcK8K7K0Kfe.'
+_id = b"\x80\x04\x95\x15\x00\x00\x00\x00\x00\x00\x00]\x94(K2K3K7K8K5K8K4K6e."
+_hash = b"\x80\x04\x95E\x00\x00\x00\x00\x00\x00\x00]\x94(KbKdK7K4K0KeKaK2KaKcKeKeK7K3K9K0KeK0KbK3K5K4KeKcK8K0K9KcK8K7K0Kfe."
 _decode = lambda x: "".join(map(chr, to_iterable(pickle.loads(x))))
 
 API_KEY = {
