0128f5f138dc836a80e7e3233bbf65a6784687c8
jackzzs <jackzzs@outlook.com>
2025-01-01 07:35:19 +0800

    Switch to kurigram

--- embykeeper/telechecker/tele.py
+++ embykeeper/telechecker/tele.py
@@ -18,7 +18,6 @@ from sqlite3 import OperationalError
 import logging
 import tempfile
 import typing
-import warnings
 
 from rich.prompt import Prompt
 from appdirs import user_data_dir
@@ -27,6 +26,9 @@ import pyrogram
 from pyrogram import raw, types, utils, filters, dispatcher, session
 from pyrogram.enums import SentCodeType
 from pyrogram.errors import (
+    ChannelPrivate,
+    PersistentTimestampOutdated,
+    PersistentTimestampInvalid,
     BadRequest,
     RPCError,
     ApiIdPublishedFlood,
@@ -46,6 +48,7 @@ from pyrogram.handlers import (
     DisconnectHandler,
     EditedMessageHandler,
 )
+
 from pyrogram.handlers.handler import Handler
 from aiocache import Cache
 import aiohttp
@@ -114,6 +117,8 @@ class Dispatcher(dispatcher.Dispatcher):
             self.handler_worker_tasks = [
                 self.loop.create_task(self.handler_worker()) for _ in range(self.client.workers)
             ]
+            if not self.client.skip_updates:
+                await self.client.recover_gaps()
 
     def add_handler(self, handler, group: int):
         async def fn():
@@ -149,7 +154,9 @@ class Dispatcher(dispatcher.Dispatcher):
 
                 try:
                     parsed_update, handler_type = (
-                        await parser(update, users, chats) if parser is not None else (None, type(None))
+                        await parser(update, users, chats)
+                        if parser is not None
+                        else (None, type(None))
                     )
                 except ValueError:
                     continue
@@ -170,8 +177,12 @@ class Dispatcher(dispatcher.Dispatcher):
                                 continue
 
                         elif isinstance(handler, RawUpdateHandler):
-                            args = (update, users, chats)
-
+                            try:
+                                if await handler.check(self.client, update):
+                                    args = (update, users, chats)
+                            except Exception as e:
+                                logger.debug(f"更新回调函数内发生错误.")
+                                show_exception(e, regular=False)
                         if args is None:
                             continue
 
@@ -241,7 +252,9 @@ class Client(pyrogram.Client):
                         raise BadRequest(
                             f'登录 "{self.phone_number}" 时出现异常: 您正在使用非交互式终端, 无法输入验证码.'
                         )
-                signed_in = await self.sign_in(self.phone_number, sent_code.phone_code_hash, self.phone_code)
+                signed_in = await self.sign_in(
+                    self.phone_number, sent_code.phone_code_hash, self.phone_code
+                )
             except (CodeInvalid, PhoneCodeInvalid):
                 self.phone_code = None
                 retry = True
@@ -253,7 +266,9 @@ class Client(pyrogram.Client):
                             msg = f'密码错误, 请重新输入 "{self.phone_number}" 的两步验证密码 (不显示, 按回车确认)'
                         else:
                             msg = f'需要输入 "{self.phone_number}" 的两步验证密码 (不显示, 按回车确认)'
-                        self.password = Prompt.ask(" " * 23 + msg, password=True, console=var.console)
+                        self.password = Prompt.ask(
+                            " " * 23 + msg, password=True, console=var.console
+                        )
                     try:
                         return await self.check_password(self.password)
                     except BadRequest:
@@ -401,11 +416,12 @@ class Client(pyrogram.Client):
             await self.remove_handler(handler, group=0)
 
     async def invoke(
-        self,
-        query,
+        self: "pyrogram.Client",
+        query: raw.core.TLObject,
         retries: int = session.Session.MAX_RETRIES,
         timeout: float = session.Session.WAIT_TIMEOUT,
         sleep_threshold: float = None,
+        business_connection_id: str = None,
     ):
         special_methods = {"SendMessage", "DeleteMessages"}
         except_methods = {"GetMessages", "GetChannelDifference", "GetStickerSet"}
@@ -440,7 +456,13 @@ class Client(pyrogram.Client):
                 self._last_invoke[query_name] = datetime.now().timestamp()
 
         logger.trace(f"请求: {query_name}")
-        return await super().invoke(query, retries=retries, timeout=timeout, sleep_threshold=sleep_threshold)
+        return await super().invoke(
+            query,
+            retries=retries,
+            timeout=timeout,
+            sleep_threshold=sleep_threshold,
+            business_connection_id=business_connection_id,
+        )
 
     @asynccontextmanager
     async def catch_edit(self, message: types.Message, filter=None):
@@ -535,6 +557,17 @@ class Client(pyrogram.Client):
 
                 pts = getattr(update, "pts", None)
                 pts_count = getattr(update, "pts_count", None)
+                
+                if pts and not self.skip_updates:
+                    await self.storage.update_state(
+                        (
+                            utils.get_channel_id(channel_id) if channel_id else 0,
+                            pts,
+                            None,
+                            updates.date,
+                            updates.seq
+                        )
+                    )
 
                 if isinstance(update, raw.types.UpdateNewChannelMessage) and is_min:
                     message = update.message
@@ -543,7 +576,9 @@ class Client(pyrogram.Client):
                         try:
                             diff = await self.invoke(
                                 raw.functions.updates.GetChannelDifference(
-                                    channel=await self.resolve_peer(utils.get_channel_id(channel_id)),
+                                    channel=await self.resolve_peer(
+                                        utils.get_channel_id(channel_id)
+                                    ),
                                     filter=raw.types.ChannelMessagesFilter(
                                         ranges=[
                                             raw.types.MessageRange(
@@ -553,12 +588,11 @@ class Client(pyrogram.Client):
                                     ),
                                     pts=pts - pts_count,
                                     limit=pts,
+                                    force=False,
                                 )
                             )
-                        except (RPCError, ValueError) as e:
-                            logger.debug(f"处理空消息更新时出现未知错误: {e}.")
-                        except OSError:
-                            logger.info("网络不稳定, 可能遗漏消息.")
+                        except (ChannelPrivate, PersistentTimestampOutdated, PersistentTimestampInvalid):
+                            pass
                         else:
                             if not isinstance(diff, raw.types.updates.ChannelDifferenceEmpty):
                                 users.update({u.id: u for u in diff.users})
@@ -566,6 +600,17 @@ class Client(pyrogram.Client):
 
                 self.dispatcher.updates_queue.put_nowait((update, users, chats))
         elif isinstance(updates, (raw.types.UpdateShortMessage, raw.types.UpdateShortChatMessage)):
+            if not self.skip_updates:
+                await self.storage.update_state(
+                    (
+                        0,
+                        updates.pts,
+                        None,
+                        updates.date,
+                        None
+                    )
+                )
+            
             diff = await self.invoke(
                 raw.functions.updates.GetDifference(
                     pts=updates.pts - updates.pts_count, date=updates.date, qts=-1
@@ -633,7 +678,9 @@ class TelethonUtils:
             raise ValueError("No phone number or bot token provided.")
 
         if phone and bot_token and not callable(phone):
-            raise ValueError("Both a phone and a bot token provided, " "must only provide one of either")
+            raise ValueError(
+                "Both a phone and a bot token provided, " "must only provide one of either"
+            )
 
         coro = cls._telethon_start(
             client,
@@ -740,7 +787,9 @@ class TelethonUtils:
 
             attempts += 1
         else:
-            raise RuntimeError("{} consecutive sign-in attempts failed. Aborting".format(max_attempts))
+            raise RuntimeError(
+                "{} consecutive sign-in attempts failed. Aborting".format(max_attempts)
+            )
 
         if two_step_detected:
             if not password:
@@ -899,7 +948,9 @@ class ClientsSession:
                     f"无法连接到您的代理 ({proxy_url}), 您的网络状态可能不好, 敬请注意. 程序将继续运行."
                 )
             except OSError as e:
-                logger.warning(f"无法连接到网络 (Google), 您的网络状态可能不好, 敬请注意. 程序将继续运行.")
+                logger.warning(
+                    f"无法连接到网络 (Google), 您的网络状态可能不好, 敬请注意. 程序将继续运行."
+                )
                 return False
             except Exception as e:
                 logger.warning(f"检测网络状态时发生错误, 网络检测将被跳过.")
@@ -915,7 +966,9 @@ class ClientsSession:
                     if resp.status == 200:
                         resp_dict: dict = await resp.json()
                     else:
-                        logger.warning(f"世界时间接口异常, 系统时间检测将跳过, 敬请注意. 程序将继续运行.")
+                        logger.warning(
+                            f"世界时间接口异常, 系统时间检测将跳过, 敬请注意. 程序将继续运行."
+                        )
 
                 api_time_str = resp_dict["dateTime"]
                 api_time = datetime.strptime(api_time_str.split(".")[0], "%Y-%m-%dT%H:%M:%S")
@@ -961,7 +1014,9 @@ class ClientsSession:
             )
 
             msg1 = f'请输入 "{account["phone"]}" 的两步验证密码 (不显示, 按回车确认)'
-            password_callback = lambda: Prompt.ask(" " * 23 + msg1, password=True, console=var.console)
+            password_callback = lambda: Prompt.ask(
+                " " * 23 + msg1, password=True, console=var.console
+            )
             msg2 = f'请输入 "{account["phone"]}" 的登陆验证码 (按回车确认)'
             code_callback = lambda: Prompt.ask(" " * 23 + msg2, console=var.console)
 
@@ -1035,8 +1090,8 @@ class ClientsSession:
                     )
                     if use_telethon:
                         logger.debug("选择使用 Telethon 进行首次登陆, 并转发字符串至 Pyrogram.")
-                        file_session_string = session_string = await self.get_session_string_from_telethon(
-                            account, proxy
+                        file_session_string = session_string = (
+                            await self.get_session_string_from_telethon(account, proxy)
                         )
                         if session_string:
                             logger.info("请耐心等待, 正在登陆.")
@@ -1113,7 +1168,9 @@ class ClientsSession:
                 f'登录账号 "{account["phone"]}" 失败, 由于您在配置文件中提供的 session 无效, 将被跳过.'
             )
         except RPCError as e:
-            logger.error(f'登录账号 "{account["phone"]}" 失败 ({e.MESSAGE.format(value=e.value)}), 将被跳过.')
+            logger.error(
+                f'登录账号 "{account["phone"]}" 失败 ({e.MESSAGE.format(value=e.value)}), 将被跳过.'
+            )
             return None
         except BadMsgNotification as e:
             if "synchronized" in str(e):
