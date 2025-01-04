1e644344a2753066c6be878b5e292010ca55cbf8
jackzzs <jackzzs@outlook.com>
2025-01-05 03:21:13 +0800

    Fix docs

--- embykeeper/cli.py
+++ embykeeper/cli.py
@@ -178,7 +178,7 @@ async def main(
 
     msg = " 您可以通过 Ctrl+C 以结束运行." if not public else ""
     logger.info(f"欢迎使用 [orange3]{__name__.capitalize()}[/]! 正在启动, 请稍等.{msg}")
-    logger.info(f"当前版本 ({__version__}) 项目页: {__url__}.")
+    logger.info(f"当前版本 ({__version__}) 项目页: {__url__}")
     logger.debug(f'命令行参数: "{" ".join(sys.argv[1:])}".')
 
     if verbosity:
