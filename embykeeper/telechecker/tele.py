4bd2a05ec76c4af8a8530e396a70ae6e0459894b
jackzzs <jackzzs@outlook.com>
2025-01-18 20:33:20 +0800

    Formatting

--- embykeeper/telechecker/tele.py
+++ embykeeper/telechecker/tele.py
@@ -216,6 +216,7 @@ class Dispatcher(dispatcher.Dispatcher):
                 logger.error("更新控制器错误.")
                 show_exception(e, regular=False)
 
+
 class FileStorage(SQLiteStorage):
     FILE_EXTENSION = ".session"
 
@@ -272,10 +273,12 @@ class FileStorage(SQLiteStorage):
                 # Old format
                 if len(self.session_string) in [self.SESSION_STRING_SIZE, self.SESSION_STRING_SIZE_64]:
                     dc_id, test_mode, auth_key, user_id, is_bot = struct.unpack(
-                        (self.OLD_SESSION_STRING_FORMAT
-                        if len(self.session_string) == self.SESSION_STRING_SIZE else
-                        self.OLD_SESSION_STRING_FORMAT_64),
-                        base64.urlsafe_b64decode(self.session_string + "=" * (-len(self.session_string) % 4))
+                        (
+                            self.OLD_SESSION_STRING_FORMAT
+                            if len(self.session_string) == self.SESSION_STRING_SIZE
+                            else self.OLD_SESSION_STRING_FORMAT_64
+                        ),
+                        base64.urlsafe_b64decode(self.session_string + "=" * (-len(self.session_string) % 4)),
                     )
 
                     await self.dc_id(dc_id)
@@ -285,12 +288,14 @@ class FileStorage(SQLiteStorage):
                     await self.is_bot(is_bot)
                     await self.date(0)
 
-                    logger.warning("You are using an old session string format. Use export_session_string to update")
+                    logger.warning(
+                        "You are using an old session string format. Use export_session_string to update"
+                    )
                     return
 
                 dc_id, api_id, test_mode, auth_key, user_id, is_bot = struct.unpack(
                     self.SESSION_STRING_FORMAT,
-                    base64.urlsafe_b64decode(self.session_string + "=" * (-len(self.session_string) % 4))
+                    base64.urlsafe_b64decode(self.session_string + "=" * (-len(self.session_string) % 4)),
                 )
 
                 await self.dc_id(dc_id)
@@ -308,6 +313,7 @@ class FileStorage(SQLiteStorage):
 
     async def delete(self):
         os.remove(self.database)
+
 
 class Client(pyrogram.Client):
     def __init__(self, *args, **kw):
